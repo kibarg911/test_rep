7c71d800849d73eb2c3e6dab6bdd725160752cfd
kibarg911 <36549227+kibarg911@users.noreply.github.com>
2018-02-17 00:27:44 +0200

    Update init_scipt.sh

--- init_scipt.sh
+++ init_scipt.sh
@@ -9,7 +9,7 @@ apt-get install ntp ntpdate -y
 
 cd /etc
 # Download NTP.CONF
-wget https://raw.githubusercontent.com/kibarg911/test_rep/master/ntp.conf.new
+wget --no-check-certificate https://raw.githubusercontent.com/kibarg911/test_rep/master/ntp.conf.new
 cp ntp.conf.new ntp.conf
 
 # Restart service
